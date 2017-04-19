2c36c7b3583ac086054389174c49f68aa291fb5c
Marco Vittorio Ciotola <marco.ciotola@globant.com>
2017-04-19 11:22:11 -0300

    project template creation

---
+++ src/main/java/com/globant/performancetests/JavaRequestSamplerClient.java
@@ -0,0 +1,7 @@
+package com.globant.performancetests;
+
+/**
+ * Created by marco.ciotola on 19/04/2017.
+ */
+public class JavaRequestSamplerClient {
+}
---
+++ src/main/java/com/globant/services/PrimeService.java
@@ -0,0 +1,7 @@
+package com.globant.services;
+
+/**
+ * Created by marco.ciotola on 19/04/2017.
+ */
+public class PrimeService {
+}
